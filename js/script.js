19b2b56d665b6f110edd0350dee9fdbbd55f1310
Miki1101 <72377140+Miki1101@users.noreply.github.com>
2022-05-19 12:48:46 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 $(function () {
 
-  $(document).on('click','a',function(event){
-		event.preventDefault();
-		var target= $(this).attr("href");
-		$('html,body').animate({
-	   scrollTop: $(target).offset().top
-		},500);
+  // $(document).on('click','a',function(event){
+	// 	event.preventDefault();
+	// 	var target= $(this).attr("href");
+	// 	$('html,body').animate({
+	//    scrollTop: $(target).offset().top
+	// 	},500);
 	   
-	  });
-
+	//   });
+    $("a").on("click", function () {
+      var hrefLink = $(this).attr("href");
+      console.log(hrefLink);
+      $("html,body").animate(
+        {
+          scrollTop: $(hrefLink).offset().top //直接到相對位置
+        },
+        800
+      );
+    });
+    
     $(document).ready(function () {
         // 捲軸偵測距離頂部超過 '數字' 才顯示按鈕
         $(window).scroll(function () {
